99843fc08858d2a5f0c66c930f00534023c9904f
Gowthaman-M <gowthaman@iamneo.ai>
2020-11-12 12:19:19 +0530

    script  changing

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,1 @@
-CREATE DATABASE IF NOT EXISTS weather;
 CREATE TABLE IF NOT EXISTS reg (email varchar(20),name varchar(20),password varchar(20),gender varchar(20),phone varchar(20),city varchar(20),country varchar(20));
